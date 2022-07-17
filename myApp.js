7e61a122eb5e56535979da1f57be8bca927782f6
5adijaa <tal7akhadija@gmail.com>
2022-07-17 14:08:07 +0100

    console log path to successfully pass test

--- myApp.js
+++ myApp.js
@@ -6,7 +6,8 @@ console.log('Hello World')
 app.get('/', (req, res) => {
   // res.send('Hello Express')
   const absolutePath = __dirname + '/views/index.html'
-  res.sendFile(__dirname + '/views/index.html')
+  console.log(absolutePath)
+  res.sendFile(absolutePath)
 })
 
 module.exports = app
