cd1e8f70a23dc115131cee4ee51ad97a1ee07106
5adijaa <tal7akhadija@gmail.com>
2022-07-19 01:06:07 +0100

    implement a root-level request logger middlware

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ console.log('Hello World')
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
 app.get('/', (req, res) => {
   // res.send('Hello Express')
   const absolutePath = __dirname + '/views/index.html'
@@ -14,7 +18,7 @@ app.get('/', (req, res) => {
   res.sendFile(absolutePath)
 })
 
-console.log(process.env.MESSAGE_STYLE)
+console.log('MESSAGE_STYLE ', process.env.MESSAGE_STYLE)
 
 app.get('/json', (req, res)=>{
   const response = process.env.MESSAGE_STYLE === 'uppercase' ? 'Hello json'.toUpperCase() : 'Hello json'
@@ -26,5 +30,6 @@ app.get('/json', (req, res)=>{
   // }
   res.send({'message': response})
 })
+
 
 module.exports = app
